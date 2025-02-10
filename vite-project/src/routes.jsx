b49be6158f711b50bbd66e13ed117d73113d7cd0
BenjaminBerhane <benjamin.berhane@chasacademy.se>
2025-02-10 12:49:17 +0100

    Skapat mappstruktur och routes

---
+++ vite-project/src/routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index:true,
+        element: <h1>Home</h1>,
+      },
+      {
+        path: "/trip/",
+        element: <h1>Alla resor</h1>,
+      },
+      {
+        path: "/tripsview/:id",
+        element: <TripView />,
+      }
+    ],
+  },
+]);
+
+export default router
