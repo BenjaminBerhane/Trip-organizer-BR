9c8d3657967f0615e3bd6d91ccf7f409af13bd8d
rebeccalindman <rebeccalindman@icloud.com>
2025-02-10 17:02:55 +0100

    removed reduntant code

--- vite-project/src/utils/storage.js
+++ vite-project/src/utils/storage.js
@@ -22,26 +22,25 @@ export function findTripIndexById(id) {
 export function saveTrip(trip) { // save trip Object to trip array, returns new array
     const savedTrips = getTrips() || []; //default to empty array
     console.log(`Saved trips:`, savedTrips);
-    let newTripsArray = savedTrips //initialized as a copy of savedTrips
+
     const existingTripIndex = findTripIndexById(trip.id); //check for existing trip in local storage
     
-
     if (existingTripIndex === -1) {
         console.log(`No previous trip found for ${trip.id}, saving a new one`);
+    
+        trip.id = uuidv4(); // generate unique ID for new trip
+        savedTrips.push(trip); // add new trip to savedTrips array
+        saveTrips(savedTrips); // replaced trips in local storage
         
-        const generatedId = uuidv4(); // generate unique ID for new trip
-        trip.id = generatedId;
-        
-        newTripsArray.push(trip); // add new trip to savedTrips array
-        saveTrips(newTripsArray); // replaced trips in local storage
         return console.log(`New trip ${trip.title} saved and added to local storage, with id: ${trip.id}`); //todo ensure correct keys
     }
+
+    // If editing an existing trip
     const oldTrip = savedTrips[existingTripIndex]; // check for existing trip
     console.log(`Trip found for ${trip.id}, trip found: ${oldTrip.title}`);
 
-    // if editing an existing trip and id already exists
-    newTripsArray[existingTripIndex] = { ...oldTrip, ...trip }; // Update the trip
-    saveTrips(newTripsArray);
+    savedTrips[existingTripIndex] = { ...oldTrip, ...trip }; // Update the trip
+    saveTrips(savedTrips);
 
     return console.log(`Edited trip. Changed title from ${oldTrip.title} to ${trip.title}, ${oldTrip.startDate} to ${trip.startDate}, ${oldTrip.endDate} to ${trip.endDate}, ${oldTrip.destination} to ${trip.destination}.`)
 
