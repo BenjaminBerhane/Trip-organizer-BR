452bf29e855d5bf957b327edac2b9221ef085f6d
Gustav Thilander <gustav.thilander@gmail.com>
2025-02-13 14:04:05 +0100

    ny sida

--- vite-project/src/components/TripForm.jsx
+++ vite-project/src/components/TripForm.jsx
@@ -1,41 +1,46 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import ActivityFormComponent from './ActivityFormComponent';
 
 const TravelForm = () => {
-  const [destination, setDestination] = useState("");
+  const [activity, setActivity] = useState({ title: "", startDate: "", endDate: "", destination: "" });
   const [destinations, setDestinations] = useState([]);
+
+  const handleChange = (field, value) => {
+    setActivity({ ...activity, [field]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (destination.trim() !== "") {
-      const newDestination = { id: uuidv4(), name: destination };
+    if (activity.title.trim() !== "" && activity.startDate.trim() !== "" && activity.endDate.trim() !== "" && activity.destination.trim() !== "") {
+      const newDestination = { id: uuidv4(), ...activity };
       console.log(`New destination added with ID: ${newDestination.id}`);
       setDestinations([...destinations, newDestination]);
-      setDestination("");
+      setActivity({ title: "", startDate: "", endDate: "", destination: "" });
     }
+  };
+
+  const handleCancel = () => {
+    setActivity({ title: "", startDate: "", endDate: "", destination: "" });
   };
 
   return (
     <div className="card-content">
-      <h1 className="title">Travel planner</h1>
-      <form onSubmit={handleSubmit} className="form">
-        <input
-          type="text"
-          placeholder="Skriv in ditt resmål"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-          className="input"
-        />
-        <button type="submit" className="button">
-          Plan trip
-        </button>
-      </form>
+      <ActivityFormComponent
+        activity={activity}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        handleCancel={handleCancel}
+        isEditing={false}
+      />
       {destinations.length > 0 && (
         <div className="result">
           <p>Your trips:</p>
           <ul>
             {destinations.map((dest) => (
-              <li key={dest.id}><strong>{dest.name}</strong></li>
+              <li key={dest.id}>
+                <strong>{dest.title}</strong> - {dest.destination} (from {dest.startDate} to {dest.endDate})
+              </li>
             ))}
           </ul>
         </div>
