8902173d230377363f1eaf247483c700d3034861
ToeCrow <thomas.kronvold@chasacademy.se>
2025-02-13 08:45:09 +0100

    TripItem done, need to do TripList??

---
+++ vite-project/src/components/TripItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const handleClickShowDetails = (id) => {
+  navigate(`/tripsview/${id}`);
+};
+
+const TripItem = () => {
+  return (
+    <section>
+      <ul>
+        {trips.map(({ id, title, fromDate, toDate, destination }) => (
+          <li 
+            key={id} 
+            onClick={() => handleClickShowDetails(id)} 
+            className='trip'
+          >
+            <div className='trip-item'><strong>Titel: </strong><br />{title}</div>
+            <div className='trip-item'><strong>Från Datum: </strong><br />{fromDate}</div>
+            <div className='trip-item'><strong>Till Datum: </strong><br />{toDate}</div>
+            <div className='trip-item'><strong>Destination: </strong><br />{destination}</div>
+            
+            <div className='trip-item trip-buttons'>
+              <button onClick={(event) => handleClickEdit(event, id)}>Ändra</button>
+              <button onClick={(event) => handleClickDelete(event, id)}>Radera</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+export default TripItem
