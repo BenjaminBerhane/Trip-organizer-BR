c42c0af06674978cd04e997cf7328a24fe7297f7
Gustav Thilander <gustav.thilander@gmail.com>
2025-02-13 13:53:43 +0100

    ID tillagt

--- vite-project/src/components/TripForm.jsx
+++ vite-project/src/components/TripForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const TravelForm = () => {
   const [destination, setDestination] = useState("");
@@ -7,37 +8,38 @@ const TravelForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (destination.trim() !== "") {
-      setDestinations([...destinations, destination]);
+      const newDestination = { id: uuidv4(), name: destination };
+      setDestinations([...destinations, newDestination]);
       setDestination("");
     }
   };
 
   return (
-        <div className="card-content">
-          <h1 className="title">Travel planner</h1>
-          <form onSubmit={handleSubmit} className="form">
-            <input
-              type="text"
-              placeholder="Skriv in ditt resmål"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-              className="input"
-            />
-            <button type="submit" className="button">
-              Plan trip
-            </button>
-          </form>
-          {destinations.length > 0 && (
-            <div className="result">
-              <p>Your trips:</p>
-              <ul>
-                {destinations.map((dest, index) => (
-                  <li key={index}><strong>{dest}</strong></li>
-                ))}
-              </ul>
-            </div>
-          )}
+    <div className="card-content">
+      <h1 className="title">Travel planner</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          placeholder="Skriv in ditt resmål"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+          className="input"
+        />
+        <button type="submit" className="button">
+          Plan trip
+        </button>
+      </form>
+      {destinations.length > 0 && (
+        <div className="result">
+          <p>Your trips:</p>
+          <ul>
+            {destinations.map((dest) => (
+              <li key={dest.id}><strong>{dest.name}</strong></li>
+            ))}
+          </ul>
         </div>
+      )}
+    </div>
   );
 };
 
