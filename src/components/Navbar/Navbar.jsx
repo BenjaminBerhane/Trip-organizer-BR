7b52740e2ec51c9d8a842cbbdaa7752e32d54b4a
rebeccalindman <rebeccalindman@icloud.com>
2025-02-14 11:00:37 +0100

    small edit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li><NavLink to="/home"></NavLink></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
