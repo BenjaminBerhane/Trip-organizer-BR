73d620305b360d354c03fe0eb92e339130af3ed1
Gustav Thilander <gustav.thilander@gmail.com>
2025-02-20 10:43:25 +0100

    settimeout, lazyloading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React, { Suspense, lazy } from 'react';
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import { Link } from 'react-router-dom';
 
 const Inspiration = lazy(() => import('../components/Inspiration/Inspiration'));
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // 1.5 sekunder
+
+    return () => clearTimeout(timer); // Rensa timeout om komponenten avmonteras
+  }, []);
+
   return (
     <>
       <h2>Välkommen hit!</h2>
@@ -12,9 +22,13 @@ function Home() {
         <Link to="/alltripsview"><button>Se alla mina planerade resor</button></Link>
         <Link to="/addtrip"><button>Skapa en ny resa</button></Link>
       </section>
-      <Suspense fallback={<div>Laddar inspiration...</div>}>
-        <Inspiration />
-      </Suspense>
+      {isLoading ? (
+        <div>Laddar inspiration...</div>
+      ) : (
+        <Suspense fallback={<div>Laddar inspiration...</div>}>
+          <Inspiration />
+        </Suspense>
+      )}
     </>
   );
 }
